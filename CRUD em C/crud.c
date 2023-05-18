e0bd04b5b75d8bedc9a80169e9920dfff04f6f62
Ricardo Iwata <102753476+ricardoiwata@users.noreply.github.com>
2023-05-17 22:36:10 -0300

    Added Editar Usuário

--- CRUD em C/crud.c
+++ CRUD em C/crud.c
@@ -69,3 +69,50 @@ void cadastrarUsuario()
 
     printf("Usuário cadastrado com sucesso.\n");
 }
+void editarUsuario()
+{
+    int id;
+    printf("Digite o ID do usuário a ser editado: ");
+    scanf("%d", &id);
+
+    int i;
+    for (i = 0; i < totalUsuarios; i++)
+    {
+        if (usuarios[i].id == id)
+        {
+            Usuario usuario = usuarios[i];
+
+            printf("Digite o novo nome completo: ");
+            fgets(usuario.nome, MAX_NAME_LENGTH, stdin);
+            usuario.nome[strcspn(usuario.nome, "\n")] = '\0';
+
+            printf("Digite o novo email: ");
+            fgets(usuario.email, MAX_EMAIL_LENGTH, stdin);
+            usuario.email[strcspn(usuario.email, "\n")] = '\0';
+
+            printf("Digite o novo sexo (Feminino, Masculino ou Indiferente): ");
+            fgets(usuario.sexo, MAX_NAME_LENGTH, stdin);
+            usuario.sexo[strcspn(usuario.sexo, "\n")] = '\0';
+
+            printf("Digite o novo endereço: ");
+            fgets(usuario.endereco, MAX_ADDRESS_LENGTH, stdin);
+            usuario.endereco[strcspn(usuario.endereco, "\n")] = '\0';
+
+            printf("Digite a nova altura (entre 1 e 2 metros): ");
+            scanf("%lf", &usuario.altura);
+            getchar();
+
+            printf("Recebeu a vacina? (1 - Sim, 0 - Não): ");
+            scanf("%d", &usuario.vacina);
+
+            usuarios[i] = usuario;
+            printf("Usuário editado com sucesso.\n");
+            return;
+        }
+    }
+
+    printf("Usuário não encontrado.\n");
+}
+  return 0;
+}
+
